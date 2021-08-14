242f2b3b5e4f62d481dcd9966d8bca5906dc4480
German Razo Rodriguez <ger9366@gmail.com>
2021-08-14 16:44:44 -0700

    remove unused data

--- src/main/java/com/service/weather/forecast/ForecastController.java
+++ src/main/java/com/service/weather/forecast/ForecastController.java
@@ -1,9 +1,6 @@
 package com.service.weather.forecast;
 
 import lombok.Data;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
